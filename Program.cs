5795baa6144adf9f59bd2525da636bd5d3972e10
Adam Poit <adam.poit@faithlife.com>
2021-06-24 22:26:46 -0700

    Various improvements to display and reliability.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using Faithlife.Build;
 using Faithlife.Utility;
 using SixLabors.Fonts;
@@ -8,141 +13,180 @@ using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using static Faithlife.Build.AppRunner;
+using static Faithlife.Build.BuildUtility;
 
 namespace rekordbox_obs
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			string EDMDirectory = "/Users/adam.poit/Music/EDM";
 
-			int windowID = 0;
+			var possibleWindowIds = new List<int>();
 			RunApp("bash", new AppRunnerSettings
 			{
-				HandleOutputLine = (line) => int.TryParse(line, out windowID),
-				Arguments = new[] { "-c", "GetWindowID rekordbox 'rekordbox'" },
+				HandleOutputLine = (line) =>
+				{
+					var match = Regex.Match(line, "\"rekordbox\".*id=(\\d+)");
+					if (match.Success)
+						possibleWindowIds.Add(int.Parse(match.Groups[1].Value));
+				},
+				Arguments = new[] { "-c", "GetWindowID rekordbox --list" },
 			});
+
+			int windowId = 0;
+			// int midpointX = 0;
+			foreach (var possibleWindowId in possibleWindowIds)
+			{
+				RunApp("screencapture", new AppRunnerSettings
+				{
+					Arguments = new[] { "-l", possibleWindowId.ToString(), "-x", "rekordbox.png" },
+				});
+
+				using var rekordboxScreenshot = Image.Load<Rgba32>("rekordbox.png");
+				var width = GetWindowWidth(rekordboxScreenshot);
+				if (width != 0)
+				{
+					windowId = possibleWindowId;
+					// midpointX = width / 2;
+				}
+			}
+			if (windowId == 0)
+				throw new InvalidOperationException("Unable to locate rekordbox window.");
+
+			var cancellationTokenSource = new CancellationTokenSource();
+			Console.CancelKeyPress += (s, e) =>
+			{
+				e.Cancel = true;
+				cancellationTokenSource.Cancel();
+			};
 
 			var currentTrack = Track.None;
 			var previousTrack = Track.None;
 			while (true)
 			{
-				RunApp("screencapture", new AppRunnerSettings
+				try
 				{
-					Arguments = new[] { "-l", windowID.ToString(), "-x", "rekordbox.png" },
-				});
+					if (cancellationTokenSource.IsCancellationRequested)
+						break;
 
-				using var rekordboxScreenshot = Image.Load<Rgba32>("rekordbox.png");
-				int x = 0;
-				int y = 0;
-				int width = 0;
-
-				bool found = false;
-				for (y = 0; y < rekordboxScreenshot.Height - 5; y++)
-				{
-					var pixelSpan = rekordboxScreenshot.GetPixelRowSpan(y);
-					for (x = 0; x < rekordboxScreenshot.Width; x++)
+					RunApp("screencapture", new AppRunnerSettings
 					{
-						var pixel = pixelSpan[x];
-						if (pixel.R == 25 && pixel.G == 25 && pixel.B == 25)
-						{
-							found = true;
-							width++;
-						}
+						Arguments = new[] { "-l", windowId.ToString(), "-x", "rekordbox.png" },
+					});
+
+					using var rekordboxScreenshot = Image.Load<Rgba32>("rekordbox.png");
+
+					using var leftSongInfo = GetSongInfoBitmap(rekordboxScreenshot, 0);
+					var leftMasterRect = GetNearestRectangle(leftSongInfo.Width * 0.93, leftSongInfo.Height * 0.55, leftSongInfo.Width * 0.07, leftSongInfo.Height * 0.45);
+					using var leftMaster = leftSongInfo.Clone(x => x.Crop(leftMasterRect));
+					bool isLeftMaster = TrackIsMaster(leftMaster);
+
+					using var rightSongInfo = GetSongInfoBitmap(rekordboxScreenshot, rekordboxScreenshot.Width / 2);
+					var rightMasterRect = GetNearestRectangle(rightSongInfo.Width * 0.93, rightSongInfo.Height * 0.55, rightSongInfo.Width * 0.07, rightSongInfo.Height * 0.45);
+					using var rightMaster = rightSongInfo.Clone(x => x.Crop(rightMasterRect));
+					bool isRightMaster = TrackIsMaster(rightMaster);
+
+					if (isLeftMaster)
+					{
+						currentTrack = Track.Left;
+					}
+					else if (isRightMaster)
+					{
+						currentTrack = Track.Right;
 					}
 
-					if (found)
-						break;
-				}
-
-				var midpointX = width / 2;
-
-				found = false;
-				var color = new Rgba32(50, 50, 50);
-				for (y = 0; y < rekordboxScreenshot.Height - 5; y++)
-				{
-					for (x = 0; x < rekordboxScreenshot.Width - 5; x++)
+					if (currentTrack != previousTrack)
 					{
-						var topLeftPixel = rekordboxScreenshot[x, y];
-						var topRightPixel = rekordboxScreenshot[x, y + 5];
-						var bottomLeftPixel = rekordboxScreenshot[x + 5, y];
-						var bottomRightPixel = rekordboxScreenshot[x + 5, y + 5];
-						if (topLeftPixel == color && topRightPixel == color && bottomLeftPixel == color && bottomRightPixel == color)
-						{
-							found = true;
-							break;
-						}
+						previousTrack = currentTrack;
+
+						await ProcessTrack(EDMDirectory, Track.Left, isLeftMaster, leftSongInfo).ConfigureAwait(false);
+						await ProcessTrack(EDMDirectory, Track.Right, isRightMaster, rightSongInfo).ConfigureAwait(false);
 					}
-
-					if (found)
-						break;
 				}
-
-				var songInfoRect = new Rectangle(x, y, rekordboxScreenshot.Width - x - 70, 85);
-				using var songInfo = rekordboxScreenshot.Clone(x => x.Crop(songInfoRect));
-				songInfo.Save("songInfo.png");
-
-				var leftMasterRect = new Rectangle(midpointX - 200, 50, 80, 30);
-				using var leftMaster = songInfo.Clone(x => x.Crop(leftMasterRect));
-				bool isLeftMaster = TrackIsMaster(leftMaster);
-
-				var rightMasterRect = new Rectangle(songInfo.Width - 90, 50, 80, 30);
-				using var rightMaster = songInfo.Clone(x => x.Crop(rightMasterRect));
-				bool isRightMaster = TrackIsMaster(rightMaster);
-
-				if (isLeftMaster)
+				catch (Exception)
 				{
-					currentTrack = Track.Left;
-				}
-				else if (isRightMaster)
-				{
-					currentTrack = Track.Right;
-				}
-
-				if (currentTrack != previousTrack)
-				{
-					previousTrack = currentTrack;
-
-					var leftSongRect = new Rectangle(115, 10, midpointX - 450, 40);
-					ProcessTrack(EDMDirectory, Track.Left, isLeftMaster, songInfo, leftSongRect);
-
-					var rightSongRect = new Rectangle(midpointX + 225, 10, midpointX - 450, 40);
-					ProcessTrack(EDMDirectory, Track.Right, isRightMaster, songInfo, rightSongRect);
+					if (!cancellationTokenSource.IsCancellationRequested)
+						throw;
 				}
 			}
+
+			foreach (var file in FindFiles("*.png*", "*.txt"))
+				File.Delete(file);
 		}
 
-		private static void ProcessTrack(string EDMDirectory, Track track, bool isTrackMaster, Image<Rgba32> songInfo, Rectangle songRect)
+		private static int GetNearestInt(double value)
+		{
+			return (int)Math.Floor(value);
+		}
+
+		private static Rectangle GetNearestRectangle(double x, double y, double width, double height)
+		{
+			return new Rectangle(GetNearestInt(x), GetNearestInt(y), GetNearestInt(width), GetNearestInt(height));
+		}
+
+		private static async Task ProcessTrack(string EDMDirectory, Track track, bool isTrackMaster, Image<Rgba32> songInfo)
 		{
 			var trackString = $"{track.ToString().ToLower()}Song";
 
+			var songRect = GetNearestRectangle(songInfo.Width * 0.08, songInfo.Height * 0.1, songInfo.Width - songInfo.Width * 0.24, songInfo.Height * 0.5);
 			using var song = songInfo.Clone(x => x.Crop(songRect));
 			var songImageFilename = $"{trackString}.png";
-			song.Save(songImageFilename);
-
-			RunApp("tesseract", new AppRunnerSettings
-			{
-				Arguments = new[] { songImageFilename, trackString },
-			});
-			var songName = File.ReadAllLines($"{trackString}.txt").First();
+			await song.SaveAsync(songImageFilename).ConfigureAwait(false);
 
 			string songFilename = null;
-			RunApp("fzf", new AppRunnerSettings
+			void RunTesseractAndFzf()
 			{
-				WorkingDirectory = EDMDirectory,
-				Arguments = new[] { "--filter", songName.Replace("...", "") },
-				HandleOutputLine = (line) => songFilename = line,
-			});
+				RunApp("tesseract", new AppRunnerSettings
+				{
+					Arguments = new[] { songImageFilename, trackString },
+				});
+				var songName = File.ReadAllLines($"{trackString}.txt").First();
+				if (songName == "Not Loaded.")
+					return;
+
+				RunApp("fzf", new AppRunnerSettings
+				{
+					WorkingDirectory = EDMDirectory,
+					Arguments = new[] { "--filter", songName.Replace("...", "") },
+					HandleOutputLine = (line) => songFilename = line,
+				});
+			}
+
+			try
+			{
+				RunTesseractAndFzf();
+			}
+			catch (BuildException)
+			{
+				RunApp("convert", new AppRunnerSettings
+				{
+					Arguments = new[] { songImageFilename, "-filter", "Catrom", "-resize", "600%", songImageFilename },
+				});
+
+				RunTesseractAndFzf();
+			}
+
+			if (songFilename == null)
+				return;
+
 			var songTags = TagLib.File.Create(System.IO.Path.Combine(EDMDirectory, songFilename));
 
+			// Customize these
+			var maxSongWidth = 960;
+			var outerPadding = 10;
+			var gradientBlend = 250;
+			var coverSize = 200;
+			var innerPadding = 5;
+			var wrapTextWidth = maxSongWidth - coverSize - gradientBlend - outerPadding * 2;
+
 			using var cover = Image.Load(songTags.Tag.Pictures[0].Data.ToArray());
-			cover.Mutate(x => x.Resize(200, 200));
+			cover.Mutate(x => x.Resize(coverSize, coverSize));
 
 			var title = songTags.Tag.Title;
 			var artists = songTags.Tag.Performers.Join(",");
 			var isPlayingText = isTrackMaster ? "NOW PLAYING" : "PREVIOUS";
-			var wrapTextWidth = 730;
 
 			var font = new Font(SystemFonts.CreateFont("Arial", 24.0f), 24.0f);
 			var boldFont = new Font(SystemFonts.CreateFont("Arial", 30.0f, FontStyle.Bold), 30.0f);
@@ -160,33 +204,39 @@ namespace rekordbox_obs
 				},
 			};
 
-			using var songMetadata = new Image<Rgba32>(960, 230 + (int)isPlayingTextSize.Height);
-			var topLine = 20 + (int)isPlayingTextSize.Height;
+			using var songMetadata = new Image<Rgba32>(maxSongWidth, coverSize + outerPadding * 3 + innerPadding * 2 + (int)isPlayingTextSize.Height);
+			var topLine = innerPadding * 2 + outerPadding * 2 + (int)isPlayingTextSize.Height;
+			var coverWithPadding = coverSize + outerPadding * 2;
+			var gradientWidth = coverWithPadding + titleSize.Width + gradientBlend;
 
 			if (track == Track.Left)
 			{
+				var gradientBrush = new LinearGradientBrush(new PointF(0, 0), new PointF(gradientWidth, 0), GradientRepetitionMode.None, new ColorStop(0, new Rgba32(0, 0, 0, 0.5f)), new ColorStop((coverWithPadding + titleSize.Width) / gradientWidth, new Rgba32(0, 0, 0, 0.35f)), new ColorStop(0.9f, new Rgba32(0, 0, 0, 0)));
 				songMetadata.Mutate(x => x
-					.DrawImage(cover, new Point(10, topLine), 1.0f)
-					.Fill(new SolidBrush(Color.Red), new RectangleF(10, 0, isPlayingTextSize.Width + 10, isPlayingTextSize.Height + 10))
-					.DrawText(new DrawingOptions(), isPlayingText, boldFont, fontColor, new PointF(15, 5))
-					.DrawText(drawingOptions, title, boldFont, fontColor, new PointF(220, topLine))
-					.DrawText(drawingOptions, artists, font, fontColor, new PointF(220, topLine + titleSize.Height))
-					.DrawText(drawingOptions, songTags.Tag.Publisher, font, fontColor, new PointF(220, topLine + titleSize.Height + artistsSize.Height)));
+					.Fill(gradientBrush, new RectangleF(0, topLine - outerPadding, gradientWidth, coverWithPadding))
+					.DrawImage(cover, new Point(outerPadding, topLine), 1.0f)
+					.Fill(new SolidBrush(Color.Red), new RectangleF(outerPadding, 0, isPlayingTextSize.Width + outerPadding, isPlayingTextSize.Height + outerPadding))
+					.DrawText(new DrawingOptions(), isPlayingText, boldFont, fontColor, new PointF(outerPadding + innerPadding, innerPadding))
+					.DrawText(drawingOptions, title, boldFont, fontColor, new PointF(coverWithPadding, topLine))
+					.DrawText(drawingOptions, artists, font, fontColor, new PointF(coverWithPadding, topLine + titleSize.Height))
+					.DrawText(drawingOptions, songTags.Tag.Publisher, font, fontColor, new PointF(coverWithPadding, topLine + titleSize.Height + artistsSize.Height)));
 			}
 			else if (track == Track.Right)
 			{
 				drawingOptions.TextOptions.HorizontalAlignment = HorizontalAlignment.Right;
 
+				var gradientBrush = new LinearGradientBrush(new PointF(maxSongWidth, 0), new PointF(maxSongWidth - gradientWidth, 0), GradientRepetitionMode.None, new ColorStop(0, new Rgba32(0, 0, 0, 0.5f)), new ColorStop((coverWithPadding + titleSize.Width) / gradientWidth, new Rgba32(0, 0, 0, 0.35f)), new ColorStop(0.9f, new Rgba32(0, 0, 0, 0)));
 				songMetadata.Mutate(x => x
-					.DrawImage(cover, new Point(750, topLine), 1.0f)
-					.Fill(new SolidBrush(Color.Red), new RectangleF(940 - isPlayingTextSize.Width, 0, isPlayingTextSize.Width + 10, isPlayingTextSize.Height + 10))
-					.DrawText(new DrawingOptions(), isPlayingText, boldFont, fontColor, new PointF(945 - isPlayingTextSize.Width, 5))
-					.DrawText(drawingOptions, title, boldFont, fontColor, new PointF(10, topLine))
-					.DrawText(drawingOptions, artists, font, fontColor, new PointF(10, topLine + titleSize.Height))
-					.DrawText(drawingOptions, songTags.Tag.Publisher, font, fontColor, new PointF(10, topLine + titleSize.Height + artistsSize.Height)));
+					.Fill(gradientBrush, new RectangleF(maxSongWidth - gradientWidth, topLine - outerPadding, gradientWidth, coverWithPadding))
+					.DrawImage(cover, new Point(maxSongWidth - coverSize - outerPadding, topLine), 1.0f)
+					.Fill(new SolidBrush(Color.Red), new RectangleF(maxSongWidth - outerPadding - innerPadding * 2 - isPlayingTextSize.Width, 0, isPlayingTextSize.Width + outerPadding, isPlayingTextSize.Height + outerPadding))
+					.DrawText(new DrawingOptions(), isPlayingText, boldFont, fontColor, new PointF(maxSongWidth - outerPadding - innerPadding - isPlayingTextSize.Width, innerPadding))
+					.DrawText(drawingOptions, title, boldFont, fontColor, new PointF(gradientBlend, topLine))
+					.DrawText(drawingOptions, artists, font, fontColor, new PointF(gradientBlend, topLine + titleSize.Height))
+					.DrawText(drawingOptions, songTags.Tag.Publisher, font, fontColor, new PointF(gradientBlend, topLine + titleSize.Height + artistsSize.Height)));
 			}
 
-			songMetadata.Save($"{trackString}Metadata.png");
+			await songMetadata.SaveAsync($"{trackString}Metadata.png").ConfigureAwait(false);
 		}
 
 		private static bool TrackIsMaster(Image<Rgba32> bitmap)
@@ -202,6 +252,76 @@ namespace rekordbox_obs
 			}
 
 			return false;
+		}
+
+		private static int GetWindowWidth(Image<Rgba32> rekordboxScreenshot)
+		{
+			int x = 0;
+			int y = 0;
+			int width = 0;
+
+			bool found = false;
+			for (y = 0; y < rekordboxScreenshot.Height; y++)
+			{
+				var pixelSpan = rekordboxScreenshot.GetPixelRowSpan(y);
+				for (x = 0; x < rekordboxScreenshot.Width; x++)
+				{
+					var pixel = pixelSpan[x];
+					if (pixel.R == 25 && pixel.G == 25 && pixel.B == 25)
+					{
+						found = true;
+						width++;
+					}
+				}
+
+				if (found)
+					break;
+			}
+
+			return width;
+		}
+
+		private static Image<Rgba32> GetSongInfoBitmap(Image<Rgba32> rekordboxScreenshot, int startX)
+		{
+			int x = startX;
+			int y = 0;
+			int width = 0;
+			int height = 0;
+			bool found = false;
+			var color = new Rgba32(50, 50, 50);
+			var black = new Rgba32(0, 0, 0);
+
+			for (y = 0; y < rekordboxScreenshot.Height; y++)
+			{
+				for (x = startX; x < rekordboxScreenshot.Width; x++)
+				{
+					var pixel = rekordboxScreenshot[x, y];
+					if (pixel == color)
+					{
+						width = 0;
+						while (rekordboxScreenshot[x + width + 1, y] == color)
+							width++;
+
+						if (width < rekordboxScreenshot.Width / 4)
+							continue;
+
+						height = 1;
+						while (rekordboxScreenshot[x + width - 2, y + height + 1] == black)
+							height++;
+
+						found = true;
+						break;
+					}
+				}
+
+				if (found)
+					break;
+			}
+
+			var songInfoRect = new Rectangle(x, y, width, height);
+			var songInfo = rekordboxScreenshot.Clone(x => x.Crop(songInfoRect));
+
+			return songInfo;
 		}
 
 		public enum Track
