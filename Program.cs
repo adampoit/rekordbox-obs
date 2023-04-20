7ccedf3c21ab38f3769ee79dbb8c268689e19a2c
Adam Poit <metalkeys404@gmail.com>
2023-04-19 20:50:35 -0700

    Support Windows.
    Includes other bug fixes. Doesn't crash when an exception is
    encountered.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,6 +12,7 @@ using FuzzySharp;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using static Faithlife.Build.AppRunner;
@@ -23,7 +24,7 @@ namespace rekordbox_obs
 	{
 		static async Task Main(string[] args)
 		{
-			string EDMDirectory = "/Users/adam.poit/Music/EDM";
+			string EDMDirectory = "/Users/adam/Music/EDM";
 
 			var songTagsLookup = new Dictionary<string, TagLib.File>();
 			foreach (var songFile in Directory.EnumerateFiles(EDMDirectory, "*.*", SearchOption.AllDirectories))
@@ -38,33 +39,12 @@ namespace rekordbox_obs
 				}
 			}
 
-			var possibleWindowIds = new List<int>();
-			RunApp("bash", new AppRunnerSettings
-			{
-				HandleOutputLine = (line) =>
-				{
-					var match = Regex.Match(line, "\"rekordbox\".*id=(\\d+)");
-					if (match.Success)
-						possibleWindowIds.Add(int.Parse(match.Groups[1].Value));
-				},
-				Arguments = new[] { "-c", "GetWindowID rekordbox --list" },
-			});
+			var processes = System.Diagnostics.Process.GetProcessesByName("rekordbox").ToList();
+			if (processes.Count > 1)
+				throw new InvalidOperationException("Multiple copies of rekordbox running!");
 
-			int windowId = 0;
-			foreach (var possibleWindowId in possibleWindowIds)
-			{
-				RunApp("screencapture", new AppRunnerSettings
-				{
-					Arguments = new[] { "-l", possibleWindowId.ToString(), "-x", "rekordbox.png" },
-				});
-
-				using var rekordboxScreenshot = Image.Load<Rgba32>("rekordbox.png");
-				var width = GetWindowWidth(rekordboxScreenshot);
-				if (width != 0)
-					windowId = possibleWindowId;
-			}
-			if (windowId == 0)
-				throw new InvalidOperationException("Unable to locate rekordbox window.");
+			var windowHandle = processes.First().MainWindowHandle;
+			GetWindowRect(windowHandle, out var rect);
 
 			var cancellationTokenSource = new CancellationTokenSource();
 			Console.CancelKeyPress += (s, e) =>
@@ -85,14 +65,9 @@ namespace rekordbox_obs
 					if (cancellationTokenSource.IsCancellationRequested)
 						break;
 
-					RunApp("screencapture", new AppRunnerSettings
-					{
-						Arguments = new[] { "-l", windowId.ToString(), "-x", "rekordbox.png" },
-					});
+					using var rekordboxScreenshot = ScreenShot(windowHandle, "rekordbox.png");
 
-					using var rekordboxScreenshot = Image.Load<Rgba32>("rekordbox.png");
-
-					using var leftSongInfo = GetSongInfoBitmap(rekordboxScreenshot, 0);
+					using var leftSongInfo = GetSongInfoBitmap(rekordboxScreenshot, Track.Left);
 					await leftSongInfo.SaveAsync("leftSongInfo.png").ConfigureAwait(false);
 					Rectangle leftMasterRect;
 					if (IsRetina)
@@ -108,7 +83,7 @@ namespace rekordbox_obs
 					await leftMaster.SaveAsync("leftMaster.png").ConfigureAwait(false);
 					bool isLeftMaster = TrackIsMaster(leftMaster);
 
-					using var rightSongInfo = GetSongInfoBitmap(rekordboxScreenshot, rekordboxScreenshot.Width / 2);
+					using var rightSongInfo = GetSongInfoBitmap(rekordboxScreenshot, Track.Right);
 					await rightSongInfo.SaveAsync("rightSongInfo.png").ConfigureAwait(false);
 					Rectangle rightMasterRect;
 					if (IsRetina)
@@ -141,10 +116,9 @@ namespace rekordbox_obs
 						await ProcessTrack(songTagsLookup, Track.Right, isRightMaster, rightSongInfo).ConfigureAwait(false);
 					}
 				}
-				catch (Exception)
+				catch (Exception ex)
 				{
-					if (!cancellationTokenSource.IsCancellationRequested)
-						throw;
+					Console.WriteLine(ex.ToString());
 				}
 			}
 
@@ -152,7 +126,7 @@ namespace rekordbox_obs
 				File.Delete(file);
 		}
 
-		private static bool IsRetina => true;
+		private static bool IsRetina => false;
 
 		private static int GetNearestInt(double value)
 		{
@@ -185,7 +159,7 @@ namespace rekordbox_obs
 
 			string RunTesseractAndGetSongName()
 			{
-				RunApp("tesseract", new AppRunnerSettings
+				RunApp("C:\\Program Files\\Tesseract-OCR\\tesseract.exe", new AppRunnerSettings
 				{
 					Arguments = new[] { "--psm", "3", songImageFilename, trackString },
 				});
@@ -201,14 +175,14 @@ namespace rekordbox_obs
 				return songMatch.Value;
 			}
 
-			RunApp("convert", new AppRunnerSettings
+			RunApp("C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\convert.exe", new AppRunnerSettings
 			{
 				Arguments = new[] { songImageFilename, "-channel", "RGB", "-negate", songImageFilename },
 			});
 
 			if (!IsRetina)
 			{
-				RunApp("convert", new AppRunnerSettings
+				RunApp("C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\convert.exe", new AppRunnerSettings
 				{
 					Arguments = new[] { songImageFilename, "-filter", "Catrom", "-resize", "600%", songImageFilename },
 				});
@@ -289,23 +263,6 @@ namespace rekordbox_obs
 			await songMetadata.SaveAsync($"{trackString}Metadata.png").ConfigureAwait(false);
 		}
 
-		private static string RemoveShortestWord(string input)
-		{
-			var inputElements = input.Split(' ');
-			var shortestElement = inputElements.OrderBy(x => x.Length).First();
-
-			var resultBuilder = new StringBuilder();
-			foreach (var element in inputElements)
-			{
-				if (element == shortestElement)
-					continue;
-
-				resultBuilder.Append(element + " ");
-			}
-
-			return resultBuilder.ToString().Trim();
-		}
-
 		private static bool TrackIsMaster(Image<Rgba32> bitmap)
 		{
 			for (int y = 0; y < bitmap.Height; y++)
@@ -321,69 +278,17 @@ namespace rekordbox_obs
 			return false;
 		}
 
-		private static int GetWindowWidth(Image<Rgba32> rekordboxScreenshot)
+		private static Image<Rgba32> GetSongInfoBitmap(Image<Rgba32> rekordboxScreenshot, Track track)
 		{
-			int x = 0;
-			int y = 0;
-			int width = 0;
-
-			bool found = false;
-			for (y = 0; y < rekordboxScreenshot.Height; y++)
+			var y = 190;
+			var x = track switch
 			{
-				var pixelSpan = rekordboxScreenshot.GetPixelRowSpan(y);
-				for (x = 0; x < rekordboxScreenshot.Width; x++)
-				{
-					var pixel = pixelSpan[x];
-					if (pixel.R == 25 && pixel.G == 25 && pixel.B == 25)
-					{
-						found = true;
-						width++;
-					}
-				}
-
-				if (found)
-					break;
-			}
-
-			return width;
-		}
-
-		private static Image<Rgba32> GetSongInfoBitmap(Image<Rgba32> rekordboxScreenshot, int startX)
-		{
-			int x = startX;
-			int y = 0;
-			int width = 0;
-			int height = 0;
-			bool found = false;
-			var color = new Rgba32(50, 50, 50);
-			var black = new Rgba32(0, 0, 0);
-
-			for (y = 0; y < rekordboxScreenshot.Height; y++)
-			{
-				for (x = startX; x < rekordboxScreenshot.Width; x++)
-				{
-					var pixel = rekordboxScreenshot[x, y];
-					if (pixel == color)
-					{
-						width = 0;
-						while (rekordboxScreenshot[x + width + 1, y] == color)
-							width++;
-
-						if (width < rekordboxScreenshot.Width / 4)
-							continue;
-
-						height = 1;
-						while (rekordboxScreenshot[x + width - 2, y + height + 1] == black)
-							height++;
-
-						found = true;
-						break;
-					}
-				}
-
-				if (found)
-					break;
-			}
+				Track.Left => 1,
+				Track.Right => rekordboxScreenshot.Width / 2 + 47,
+				_ => throw new InvalidOperationException($"Invalid track kind: {track}!"),
+			};
+			var width = rekordboxScreenshot.Width / 2 - 63;
+			var height = 44;
 
 			var songInfoRect = new Rectangle(x, y, width, height);
 			var songInfo = rekordboxScreenshot.Clone(x => x.Crop(songInfoRect));
@@ -391,11 +296,219 @@ namespace rekordbox_obs
 			return songInfo;
 		}
 
+		private static Image<Rgba32> ScreenShot(IntPtr hWnd, string fileName)
+		{
+			GetWindowRect(hWnd, out var rc);
+			var hWndDc = GetDC(hWnd);
+			var hMemDc = CreateCompatibleDC(hWndDc);
+			var hBitmap = CreateCompatibleBitmap(hWndDc, rc.Width, rc.Height);
+			SelectObject(hMemDc, hBitmap);
+
+			BitBlt(hMemDc, 0, 0, rc.Width, rc.Height, hWndDc, 0, 0, TernaryRasterOperations.SRCCOPY);
+			var bitmap = System.Drawing.Bitmap.FromHbitmap(hBitmap);
+
+			DeleteObject(hBitmap);
+			ReleaseDC(hWnd, hWndDc);
+			DeleteDC(hMemDc);
+
+			using var memoryStream = new MemoryStream();
+			bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+			memoryStream.Seek(0, SeekOrigin.Begin);
+
+			return Image.Load<Rgba32>(memoryStream, new JpegDecoder());
+		}
+
+		private enum TernaryRasterOperations : uint
+		{
+			/// <summary>dest = source</summary>
+			SRCCOPY = 0x00CC0020,
+			/// <summary>dest = source OR dest</summary>
+			SRCPAINT = 0x00EE0086,
+			/// <summary>dest = source AND dest</summary>
+			SRCAND = 0x008800C6,
+			/// <summary>dest = source XOR dest</summary>
+			SRCINVERT = 0x00660046,
+			/// <summary>dest = source AND (NOT dest)</summary>
+			SRCERASE = 0x00440328,
+			/// <summary>dest = (NOT source)</summary>
+			NOTSRCCOPY = 0x00330008,
+			/// <summary>dest = (NOT src) AND (NOT dest)</summary>
+			NOTSRCERASE = 0x001100A6,
+			/// <summary>dest = (source AND pattern)</summary>
+			MERGECOPY = 0x00C000CA,
+			/// <summary>dest = (NOT source) OR dest</summary>
+			MERGEPAINT = 0x00BB0226,
+			/// <summary>dest = pattern</summary>
+			PATCOPY = 0x00F00021,
+			/// <summary>dest = DPSnoo</summary>
+			PATPAINT = 0x00FB0A09,
+			/// <summary>dest = pattern XOR dest</summary>
+			PATINVERT = 0x005A0049,
+			/// <summary>dest = (NOT dest)</summary>
+			DSTINVERT = 0x00550009,
+			/// <summary>dest = BLACK</summary>
+			BLACKNESS = 0x00000042,
+			/// <summary>dest = WHITE</summary>
+			WHITENESS = 0x00FF0062
+		}
+
+		[DllImport("gdi32.dll")]
+		private static extern bool BitBlt(IntPtr hdc, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSrc, int nXSrc, int nYSrc, TernaryRasterOperations dwRop);
+
+		[DllImport("gdi32.dll", ExactSpelling = true, PreserveSig = true, SetLastError = true)]
+		static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+
+		[DllImport("gdi32.dll")]
+		private static extern bool DeleteObject(IntPtr hObject);
+
+		[DllImport("gdi32.dll")]
+		private static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+
+		[DllImport("gdi32.dll", SetLastError = true)]
+		private static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr GetDC(IntPtr hWnd);
+
+		[DllImport("user32.dll")]
+		private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+		[DllImport("gdi32.dll")]
+		private static extern bool DeleteDC(IntPtr hDC);
+
+		[DllImport("user32.dll")]
+		private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
 		public enum Track
 		{
 			Left,
 			Right,
 			None,
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct RECT
+		{
+			private int _Left;
+			private int _Top;
+			private int _Right;
+			private int _Bottom;
+
+			public RECT(RECT Rectangle) : this(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom)
+			{
+			}
+			public RECT(int Left, int Top, int Right, int Bottom)
+			{
+				_Left = Left;
+				_Top = Top;
+				_Right = Right;
+				_Bottom = Bottom;
+			}
+
+			public int X
+			{
+				get { return _Left; }
+				set { _Left = value; }
+			}
+			public int Y
+			{
+				get { return _Top; }
+				set { _Top = value; }
+			}
+			public int Left
+			{
+				get { return _Left; }
+				set { _Left = value; }
+			}
+			public int Top
+			{
+				get { return _Top; }
+				set { _Top = value; }
+			}
+			public int Right
+			{
+				get { return _Right; }
+				set { _Right = value; }
+			}
+			public int Bottom
+			{
+				get { return _Bottom; }
+				set { _Bottom = value; }
+			}
+			public int Height
+			{
+				get { return _Bottom - _Top; }
+				set { _Bottom = value + _Top; }
+			}
+			public int Width
+			{
+				get { return _Right - _Left; }
+				set { _Right = value + _Left; }
+			}
+			public Point Location
+			{
+				get { return new Point(Left, Top); }
+				set
+				{
+					_Left = value.X;
+					_Top = value.Y;
+				}
+			}
+			public Size Size
+			{
+				get { return new Size(Width, Height); }
+				set
+				{
+					_Right = value.Width + _Left;
+					_Bottom = value.Height + _Top;
+				}
+			}
+
+			public static implicit operator Rectangle(RECT Rectangle)
+			{
+				return new Rectangle(Rectangle.Left, Rectangle.Top, Rectangle.Width, Rectangle.Height);
+			}
+			public static implicit operator RECT(Rectangle Rectangle)
+			{
+				return new RECT(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom);
+			}
+			public static bool operator ==(RECT Rectangle1, RECT Rectangle2)
+			{
+				return Rectangle1.Equals(Rectangle2);
+			}
+			public static bool operator !=(RECT Rectangle1, RECT Rectangle2)
+			{
+				return !Rectangle1.Equals(Rectangle2);
+			}
+
+			public override string ToString()
+			{
+				return "{Left: " + _Left + "; " + "Top: " + _Top + "; Right: " + _Right + "; Bottom: " + _Bottom + "}";
+			}
+
+			public override int GetHashCode()
+			{
+				return ToString().GetHashCode();
+			}
+
+			public bool Equals(RECT Rectangle)
+			{
+				return Rectangle.Left == _Left && Rectangle.Top == _Top && Rectangle.Right == _Right && Rectangle.Bottom == _Bottom;
+			}
+
+			public override bool Equals(object Object)
+			{
+				if (Object is RECT)
+				{
+					return Equals((RECT)Object);
+				}
+				else if (Object is Rectangle)
+				{
+					return Equals(new RECT((Rectangle)Object));
+				}
+
+				return false;
+			}
 		}
 	}
 }
